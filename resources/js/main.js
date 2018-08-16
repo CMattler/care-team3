07cc8af2c5d6dc24f2f92a77bf0fe1ce97a2be4e
cmattler <godruma.goura@gmail.com>
2018-08-16 13:28:53 +0530

    second commit.

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll:1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    dots: true
+  });
+
+});
